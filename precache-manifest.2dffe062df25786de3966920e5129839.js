a90b5fb35bf175638e4e6f462748d63f1bd491ed
Nien <wolfzxcv@gmail.com>
2019-07-08 21:27:59 +0100

    Updates

---
+++ precache-manifest.2dffe062df25786de3966920e5129839.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31a0aec8514a87b83b2879eb86ec175",
+    "url": "/React-Hook-todolist-LocalStorage-uuid/index.html"
+  },
+  {
+    "revision": "be5472fed1d195adf1ec",
+    "url": "/React-Hook-todolist-LocalStorage-uuid/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "00306c330573d8f04785",
+    "url": "/React-Hook-todolist-LocalStorage-uuid/static/js/2.08d8e669.chunk.js"
+  },
+  {
+    "revision": "be5472fed1d195adf1ec",
+    "url": "/React-Hook-todolist-LocalStorage-uuid/static/js/main.2def5fcb.chunk.js"
+  },
+  {
+    "revision": "bf7aabd15f878bf57614",
+    "url": "/React-Hook-todolist-LocalStorage-uuid/static/js/runtime~main.112cd810.js"
+  }
+]);
